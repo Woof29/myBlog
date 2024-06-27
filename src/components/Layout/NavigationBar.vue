8c6ab1e16b026be8048cbad63be10a8b44775385
Kevin <kevin8003129@gmail.com>
2024-06-28 01:17:50 +0800

    update: nav style

--- src/components/Layout/NavigationBar.vue
+++ src/components/Layout/NavigationBar.vue
@@ -1,111 +1,114 @@
 <script setup></script>
 
 <template>
-  <!-- <transition name="fade" appear> -->
-  <nav>
-    <ul>
-      <li>
-        <RouterLink to="/">
-          <div class="home icon"></div>
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/about">
-          <div class="about icon"></div>
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/blog">
-          <div class="blog icon"></div>
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/portfolio">
-          <div class="portfolio icon"></div>
-        </RouterLink>
-      </li>
-    </ul>
-  </nav>
-  <!-- </transition> -->
+	<!-- <transition name="fade" appear> -->
+	<nav>
+		<ul>
+			<li>
+				<RouterLink to="/">
+					<div class="home icon"></div>
+				</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/about">
+					<div class="about icon"></div>
+				</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/blog">
+					<div class="blog icon"></div>
+				</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/portfolio">
+					<div class="portfolio icon"></div>
+				</RouterLink>
+			</li>
+		</ul>
+	</nav>
+	<!-- </transition> -->
 </template>
 
 <style lang="scss" scoped>
-@import "@/style/main.scss";
+@import '@/style/main.scss';
 nav {
-  width: 76px;
-  min-height: 100vh;
-  position: fixed;
-  bottom: 0;
-  box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.7);
-  z-index: 99;
-  @include rwd($rwd-level-one) {
-    width: 100%;
-    height: 76px;
-  }
-  ul {
-    width: 100%;
-    height: 100%;
-    padding: 32px 8px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 32px;
-    @include rwd($rwd-level-one) {
-      flex-direction: row;
-      gap: unset;
-      justify-content: space-around;
-    }
-    li {
-      a {
-        width: 50px;
-        height: 50px;
-        padding: 8px 12px;
-        display: block;
-        position: relative;
-        transition: all 0.3s ease-out;
-        &:hover {
-          border-radius: 8px;
-          background: rgb(255, 255, 255, 0.1);
-        }
-        .icon {
-          width: 100%;
-          height: 100%;
-          &.home {
-            background: url("@/assets/icon/home_passive.svg") no-repeat center;
-          }
-          &.about {
-            background: url("@/assets/icon/about_passive.svg") no-repeat center;
-          }
-          &.portfolio {
-            background: url("@/assets/icon/portfolio_passive.svg") no-repeat
-              center;
-          }
-          &.blog {
-            background: url("@/assets/icon/blog_passive.svg") no-repeat center;
-          }
-        }
-        &.router-link-active {
-          .icon {
-            width: 100%;
-            height: 100%;
-            &.home {
-              background: url("@/assets/icon/home_active.svg") no-repeat center;
-            }
-            &.about {
-              background: url("@/assets/icon/about_active.svg") no-repeat center;
-            }
-            &.portfolio {
-              background: url("@/assets/icon/portfolio_active.svg") no-repeat
-                center;
-            }
-            &.blog {
-              background: url("@/assets/icon/blog_active.svg") no-repeat center;
-            }
-          }
-        }
-      }
-    }
-  }
+	width: 76px;
+	min-height: 100vh;
+	position: fixed;
+	bottom: 0;
+	box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.7);
+	z-index: 99;
+	@include rwd($rwd-level-one) {
+		width: 100%;
+		height: 76;
+		min-height: auto;
+	}
+	ul {
+		width: 100%;
+		height: 100vh;
+		padding: 32px 8px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 32px;
+		@include rwd($rwd-level-one) {
+			height: 100%;
+			padding: 8px;
+			flex-direction: row;
+			gap: unset;
+			justify-content: space-around;
+		}
+		li {
+			a {
+				width: 50px;
+				height: 50px;
+				padding: 8px 12px;
+				display: block;
+				position: relative;
+				transition: all 0.3s ease-out;
+				&:hover {
+					border-radius: 8px;
+					background: rgb(255, 255, 255, 0.1);
+				}
+				.icon {
+					width: 100%;
+					height: 100%;
+					&.home {
+						background: url('@/assets/icon/home_passive.svg') no-repeat center;
+					}
+					&.about {
+						background: url('@/assets/icon/about_passive.svg') no-repeat center;
+					}
+					&.portfolio {
+						background: url('@/assets/icon/portfolio_passive.svg') no-repeat
+							center;
+					}
+					&.blog {
+						background: url('@/assets/icon/blog_passive.svg') no-repeat center;
+					}
+				}
+				&.router-link-active {
+					.icon {
+						width: 100%;
+						height: 100%;
+						&.home {
+							background: url('@/assets/icon/home_active.svg') no-repeat center;
+						}
+						&.about {
+							background: url('@/assets/icon/about_active.svg') no-repeat center;
+						}
+						&.portfolio {
+							background: url('@/assets/icon/portfolio_active.svg') no-repeat
+								center;
+						}
+						&.blog {
+							background: url('@/assets/icon/blog_active.svg') no-repeat center;
+						}
+					}
+				}
+			}
+		}
+	}
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-import { createApp } from "vue";
-import "@/style/main.scss";
-import App from "./App.vue";
-import router from "./router/router";
-import Vue from "vue";
-import Vue2Editor from "vue2-editor";
-
-Vue.use(Vue2Editor);
+import { createApp } from 'vue';
+import '@/style/main.scss';
+import App from './App.vue';
+import router from './router/router';
+import Vue2Editor from 'vue2-editor';
 
 const app = createApp(App);
+app.use(Vue2Editor);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
