1efd2d2b965011f38b1a180a17ab2f6c0af2822e
Kevin <kevin8003129@gmail.com>
2024-07-01 01:33:30 +0800

    update: portfolio style

--- src/components/YouTubePlayer.vue
+++ src/components/YouTubePlayer.vue
@@ -1,67 +1,68 @@
 <script setup>
-import YouTubePlayer from "youtube-player";
+import YouTubePlayer from 'youtube-player';
 
 const props = defineProps({
-  id: { type: String, default: "" },
-  src: { type: String, required: true },
-  width: { type: Number, default: 0 },
-  height: { type: Number, default: 0 },
-  playerVars: {
-    type: Object,
-    default() {
-      return {};
-    },
-  },
+	id: { type: String, default: '' },
+	src: { type: String, required: true },
+	width: { type: Number, default: 0 },
+	height: { type: Number, default: 0 },
+	playerVars: {
+		type: Object,
+		default() {
+			return {};
+		},
+	},
 });
 
-const emit = defineEmits(["ended", "play", "pause"]);
+const emit = defineEmits(['ended', 'play', 'pause']);
 
 onMounted(() => {
-  initPlayer();
-  loadPlayer();
-  addStateChange();
+	initPlayer();
+	loadPlayer();
+	addStateChange();
 });
 
 onBeforeUnmount(() => {
-  removeStateChange();
-  player && player.destroy();
-  player = null;
+	removeStateChange();
+	player && player.destroy();
+	player = null;
 });
 
 const getVideoId = () => {
-  try {
-    return new URL(props.src).searchParams.get("v") || "";
-  } catch (error) {
-    return "";
-  }
+	try {
+		return new URL(props.src).searchParams.get('v') || '';
+	} catch (error) {
+		return '';
+	}
 };
 
 let player = null;
 const initPlayer = () => {
-  try {
-    player = YouTubePlayer(`youtube-${props.id}`, {
-      videoId: getVideoId(),
-      width: props.width,
-      height: props.height,
-      // playerVars 播放器其它参数配置
-      playerVars: {
-        playsinline: 1,
-        fs: 0,
-        modestbranding: 1,
-        ...props.playerVars,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		player = YouTubePlayer(`youtube-${props.id}`, {
+			videoId: getVideoId(),
+			width: props.width,
+			height: props.height,
+			// playerVars 播放器其它参数配置
+			playerVars: {
+				playsinline: 1,
+				fs: 0,
+				modestbranding: 1,
+				autoplay: 0,
+				...props.playerVars,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const loadPlayer = () => {
-  try {
-    player.loadVideoById(getVideoId());
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		player.loadVideoById(getVideoId());
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const play = () => player && player.playVideo();
@@ -70,42 +71,42 @@ const pause = () => player && player.pauseVideo();
 // -1（未开始）0（已结束）1（正在播放）2（已暂停）3（正在缓冲）5（视频已插入）
 let stateChangeListener;
 const addStateChange = () => {
-  stateChangeListener =
-    player &&
-    player.on("stateChange", (event) => {
-      if (event.data === 0) emit("ended");
-      if (event.data === 1) emit("play");
-      if (event.data === 2) emit("pause");
-    });
+	stateChangeListener =
+		player &&
+		player.on('stateChange', (event) => {
+			if (event.data === 0) emit('ended');
+			if (event.data === 1) emit('play');
+			if (event.data === 2) emit('pause');
+		});
 };
 const removeStateChange = () => {
-  if (stateChangeListener && player) player.off(stateChangeListener);
+	if (stateChangeListener && player) player.off(stateChangeListener);
 };
 
 watch(
-  () => props.src,
-  () => loadPlayer()
+	() => props.src,
+	() => loadPlayer()
 );
 
 watch(
-  () => [props.width, props.height],
-  () => {
-    player?.setSize && player.setSize(props.width, props.height);
-  }
+	() => [props.width, props.height],
+	() => {
+		player?.setSize && player.setSize(props.width, props.height);
+	}
 );
 
 defineExpose({ play, pause });
 </script>
 
 <template>
-  <div class="youtube-video">
-    <div :id="'youtube-' + id"></div>
-  </div>
+	<div class="youtube-video">
+		<div :id="'youtube-' + id"></div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .youtube-video {
-  width: 100%;
-  overflow: hidden;
+	width: 100%;
+	overflow: hidden;
 }
 </style>
--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,39 +1,84 @@
 <script setup>
-import NavigationBar from "../components/Layout/NavigationBar.vue";
+import NavigationBar from '../components/Layout/NavigationBar.vue';
 </script>
 
 <template>
-  <main>
-    <div class="container">
-      <div class="title">Portfolio</div>
+	<main>
+		<div class="container">
+			<div class="title">
+				<span>Portfolio</span>
+			</div>
 
-      <ul class="tabList">
-        <li>
-          <span>Web</span>
-        </li>
-        <li>
-          <span>Video</span>
-        </li>
-        <li>
-          <span>Photography</span>
-        </li>
-        <li>
-          <span>Graphic</span>
-        </li>
-      </ul>
+			<ul class="tabList">
+				<li class="active">
+					<span>Web</span>
+				</li>
+				<li>
+					<span>Video</span>
+				</li>
+				<li>
+					<span>Photography</span>
+				</li>
+				<li>
+					<span>Graphic</span>
+				</li>
+			</ul>
 
-      <div class="boxList">
-        <div class="BLcard" v-for="(item, index) in 5" :key="index">
-          <div class="cardPic">
-            <img src="https://fakeimg.pl/300x200/660" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <NavigationBar />
-  </main>
+			<div class="boxList">
+				<div class="BLcard" v-for="(item, index) in 5" :key="index">
+					<div class="cardPic">
+						<img src="https://fakeimg.pl/300x200/660" />
+					</div>
+				</div>
+			</div>
+		</div>
+		<NavigationBar />
+	</main>
 </template>
 
 <style lang="scss" scoped>
-@import "@/style/main.scss";
+@import '@/style/main.scss';
+.container {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	/* align-items: flex-start; */
+	gap: 16px;
+}
+.title {
+	width: 100%;
+	/* padding: 8px 16px; */
+	span {
+		@include fontStyle(32, 700, #fff);
+	}
+}
+.tabList {
+	width: 100%;
+	margin-bottom: 8px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	gap: 32px;
+	/* overflow-x: auto; */
+	/* overflow-y: hidden; */
+	&::-webkit-scrollbar {
+		display: none;
+	}
+	li {
+		padding-bottom: 2px;
+		position: relative;
+		@include fontStyle(16, 400, #4d4d4d);
+		&.active {
+			@include fontStyle(16, 700, #fff);
+			&::before {
+				content: '';
+				width: 100%;
+				height: 2px;
+				background: #fff;
+				position: absolute;
+				@include pos(bl, 0);
+			}
+		}
+	}
+}
 </style>
