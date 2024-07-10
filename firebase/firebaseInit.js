4cb761f3b11dc0373dade25016df2c6ed3539464
Kevin Chen <kevin8003129@gmail.com>
2024-07-10 16:30:18 +0800

    update: firebase config

--- firebase/firebaseInit.js
+++ firebase/firebaseInit.js
@@ -3,14 +3,13 @@ import { getFirestore, serverTimestamp, updateDoc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEjQEk1YGk5hxZ0URwdMf9FGI-8Txc5YM",
-  authDomain: "myblog-21862.firebaseapp.com",
-  projectId: "myblog-21862",
-  storageBucket: "myblog-21862.appspot.com",
-  messagingSenderId: "787745156140",
-  appId: "1:787745156140:web:7e4c66c9e2eadc1d8fb589",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const storage = getStorage(app, "gs://my-custom-bucket");
